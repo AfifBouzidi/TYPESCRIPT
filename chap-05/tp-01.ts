39aaea6695eb6e9ba6f52b2501b58cfd80e7bab0
AfifBouzidi <afif.bouzidi@gmail.com>
2018-09-01 01:35:52 +0100

    Arrow Functions

---
+++ chap-05/tp-01.ts
@@ -0,0 +1,9 @@
+let add= (x,y)=>{return x+y};
+let sub= (x,y)=>{return x-y};
+let mul= (x,y)=>{return x*y};
+let div= (x,y)=>{return x/y};
+
+console.log(add(1,2));
+console.log(sub(1,2));
+console.log(mul(1,2));
+console.log(div(1,2));
