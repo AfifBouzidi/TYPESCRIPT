963a6ed766c4bbe50ae46a0601a000a9ebf11021
AfifBouzidi <afif.bouzidi@gmail.com>
2018-09-01 01:34:32 +0100

    Types

---
+++ chap-03/tp-01.ts
@@ -0,0 +1,6 @@
+let a: number;
+let b: boolean;
+let c:string;
+let d:any;
+let e:any[]=[1,true,'hello']
+let f:number[]=[0,1,2,3];
---
+++ chap-03/tp-02.ts
@@ -0,0 +1,7 @@
+enum Colors{
+    RED =1, BLUE=2,GREEN=3
+  }
+  
+  let colors = Colors.BLUE;
+  
+  console.log(colors);
